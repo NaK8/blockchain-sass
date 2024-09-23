1d2d201ab76f2adc6ea49e57c103c5f815b493d4
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-09-24 02:21:07 +0500

    init

---
+++ src/sections/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-between"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
